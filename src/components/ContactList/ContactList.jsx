c01a4d506e8404eda04c31456c5bd48a99c957db
sydorovay <sydorovay@gmail.com>
2024-09-10 12:20:02 +0200

    Upate ContactList
    add selectNameFilter
    add a check if the array is empty

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,20 +1,25 @@
 import { useSelector } from 'react-redux';
 import { selectContacts } from '../../redux/contactsSlice';
+import { selectNameFilter } from '../../redux/filtersSlice'; //селектор для фільтра
 import Contact from '../Contact/Contact';
 import css from './ContactList.module.css';
 
 const ContactList = () => {
-  const contacts = useSelector(selectContacts); 
+  const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter); 
 
-  // Перевірка на випадок, якщо contacts не є масивом
   if (!Array.isArray(contacts)) {
     return <div>Error: Contacts data is not an array</div>;
   }
 
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <ul className={css.contactList}>
-      {contacts.length > 0 ? (
-        contacts.map(contact => (
+      {filteredContacts.length > 0 ? (
+        filteredContacts.map(contact => (
           <Contact key={contact.id} contact={contact} />
         ))
       ) : (
